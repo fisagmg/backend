886d549b1c633a231e5d8a6323ff4fe168072fdd
박여명 <qkrduaud123@gmail.com>
2025-11-28 09:59:05 +0900

    [#37-CVE학습페이지에 필터링, 개인 대시보드 구현을 위한 로직 추가]
    - CVE학습 페이지에 필터링 추가
    - 개인화 대시보드 추가
    - bin폴더 gitignore에 추가
    - 불필요한 sql 파일 삭제

--- add_medium_cves.sql
+++
@@ -1,4 +0,0 @@
--- Medium 난이도 CVE 2개 추가 (CVSS 4.0~6.9)
-INSERT INTO cve (cvss_score, name, year, num, related_domain, lab_os, outline) VALUES
-(5.3, 'CVE-2024-33891', 2024, 33891, 'WEB', 'Ubuntu', 'WordPress Plugin XSS 취약점'),
-(6.1, 'CVE-2023-28475', 2023, 28475, 'Application', 'Linux', 'Node.js 패키지 정보 노출 취약점');
